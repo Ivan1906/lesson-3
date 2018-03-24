38320ca961d16f25f2789bfb554bbbd446565d82
Ivan1906 <yanik.ivan@ukr.net>
2018-03-24 19:38:13 +0200

    Add foles component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import DBpost from './data.json';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {count: 10, db: DBpost};
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <h1>{this.state.count}</h1>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
